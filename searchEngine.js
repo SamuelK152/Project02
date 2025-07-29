3d25be685082292e614b15e3f85ebc41e800ac16
Sam <samuelkehoe96@gmail.com>
2025-07-28 21:00:57 -0700

    Styles

--- searchEngine.js
+++ searchEngine.js
@@ -1,22 +1,57 @@
+const apiKey = "mgMtRKqOssbqEmUBEyMMD3uuJf2UGuTy";
+
+const categoriesButton = document.getElementById("js-categories-button");
+categoriesButton.addEventListener("click", fetchCategories);
+
+let categoriesVisible = false;
+
 async function fetchCategories() {
-    try{
-    const data = await fetch('https://api.giphy.com/v1/gifs/categories?api_key=mgMtRKqOssbqEmUBEyMMD3uuJf2UGuTy');
-    const response = await data.json();
-    const categories = response.data;
-    const list = document.getElementById('category-list');
-    categories.forEach(cat => {
-    const item = document.createElement('li');
-    item.textContent = cat.name;
-    list.appendChild(item);
-});
+    const list = document.getElementById("js-category-list");
+
+    // Toggle visibility
+    if (categoriesVisible) {
+        list.innerHTML = "";
+        categoriesVisible = false;
+        return;
+    }
+
+    try {
+        const data = await fetch(
+            `https://api.giphy.com/v1/gifs/categories?api_key=${apiKey}`
+        );
+        const response = await data.json();
+        const categories = response.data;
+
+        // Categories Container
+        categories.forEach((cat) => {
+            const item = document.createElement("li");
+            item.textContent = cat.name;
+            item.classList.add("category-item");
+
+            // Subcategories Container
+            const subList = document.createElement("ul");
+            subList.style.display = "none";
+            subList.classList.add("subcategory-list");
+
+            cat.subcategories.forEach((sub) => {
+                const subItem = document.createElement("li");
+                subItem.textContent = sub.name;
+                subList.appendChild(subItem);
+                subItem.classList.add("subcategory-list-item");  //inprog
+            });
+
+            // Toggle subcategories on click
+            item.addEventListener("click", () => {
+                subList.style.display =
+                    subList.style.display === "none" ? "grid" : "none";
+            });
+
+            list.appendChild(item);
+            list.appendChild(subList);
+        });
+
+        categoriesVisible = true;
     } catch (error) {
-    console.error('Error fetching categories:', error);
+        console.error("Error fetching categories:", error);
+    }
 }
-}
-
-function loadPage() {
-    fetchCategories();
-};
-
-loadPage();
-
